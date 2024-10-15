47bc2c98cf46fae6c606e0a2675685b389edfe34
Mace-X6 <mees@vansteijn.info>
2024-10-16 00:10:45 +0200

    integrated in myprintf -> testing

--- my_printf.s
+++ my_printf.s
@@ -1,11 +1,11 @@
-.data
-#   msg: 
-#       .asciz "Hello World!"
-#        len = . - msg
-
 .text
+.data
+string: .asciz "My name is %s. I think I’ll get a %u for my exam. What does %r do? And %%?"
 
-.global my_printf
+
+.global main
+main:
+
 
 my_printf:
     #   prologue
@@ -95,12 +95,160 @@ my_printf:
         # if current_char == s ? handle it : jump to not_s
         cmp     %r13b,     $115
         jne      not_s
+
+            pushq   %rax
+            pushq   %rcx 
+            pushq   %rdx 
+            pushq   %rdi 
+            pushq   %rsi 
+            pushq   %r8
+            pushq   %r9
+            pushq   %r10
+            pushq   %r11
+            # i have no fucking clue which of this im using in the rest of my loop because i forgot how 
+            # i wrote it and this bullshit is unreadable so im pushing everyhting its allright
+
+            #save r15
+            pushq   %r15
+            pushq   %r15
+
+            # save sp
+            movq    %rsp,   %r15
+
+            movq    %r14,   %rdi    # passing arg number to subroutine
+            
+            call get_argument_n
+            # rax contains address of string
+
+            # pass args
+            movq    %rax, %rdi
+            movq    %rsp,   %rsi
+
+            #reserve space for response
+            subq    $24,    %rsp
+
+            call    stringify_signed_int
+
+            xorq    %r8,    %r8 # will hold amount of chars written
+            # r15 is the starting pos
+            loop:
+            xorq    %rdi,   %rdi    # clr rdi
+            movb    (%r15), %dil    # arguments for print call
+
+            pushq   %rax            # save rax because my print char fucks with it
+            pushq   %rax
+
+            call    print_char
+
+            popq    %rax            # reinstate rax
+            popq    %rax
+
+            inc     %r15            # next char
+            inc     %r8
+
+            cmp     %r8,    %rax
+            # if r8 == rax exit loop
+            # else
+            jg      loop
+
+            addq    $24,    %rsp
+            popq    %r15            # restore everything
+            popq    %r15
+
+            # okay since i pushed all that junk i restore everything here (i hope i dont run out of stack)
+            pushq   %r11
+            pushq   %r10
+            pushq   %r9
+            pushq   %r8
+            pushq   %rsi 
+            pushq   %rdi 
+            pushq   %rdx 
+            pushq   %rcx 
+            pushq   %rax
+            
+            inc     %rdi                # increment current_char_index
+            jmp     print_loop          # loop
+
         not_s:
     
 
         # if current_char == u ? handle it : jump to not_u
         cmp     %r13b,     $117
         jne      not_u
+
+            pushq   %rax
+            pushq   %rcx 
+            pushq   %rdx 
+            pushq   %rdi 
+            pushq   %rsi 
+            pushq   %r8
+            pushq   %r9
+            pushq   %r10
+            pushq   %r11
+            # i have no fucking clue which of this im using in the rest of my loop because i forgot how 
+            # i wrote it and this bullshit is unreadable so im pushing everyhting its allright
+
+            #save r15
+            pushq   %r15
+            pushq   %r15
+
+            # save sp
+            movq    %rsp,   %r15
+
+            movq    %r14,   %rdi    # passing arg number to subroutine
+            
+            call get_argument_n
+            # rax contains address of string
+
+            # pass args
+            movq    %rax, %rdi
+            movq    %rsp,   %rsi
+
+            #reserve space for response
+            subq    $24,    %rsp
+
+            call    stringify_unsigned_int
+
+            xorq    %r8,    %r8 # will hold amount of chars written
+            # r15 is the starting pos
+            loop:
+            xorq    %rdi,   %rdi    # clr rdi
+            movb    (%r15), %dil    # arguments for print call
+
+            pushq   %rax            # save rax because my print char fucks with it
+            pushq   %rax
+
+            call    print_char
+
+            popq    %rax            # reinstate rax
+            popq    %rax
+
+            inc     %r15            # next char
+            inc     %r8
+
+            cmp     %r8,    %rax
+            # if r8 == rax exit loop
+            # else
+            jg      loop
+
+            addq    $24,    %rsp
+            popq    %r15            # restore everything
+            popq    %r15
+
+            # okay since i pushed all that junk i restore everything here (i hope i dont run out of stack)
+            pushq   %r11
+            pushq   %r10
+            pushq   %r9
+            pushq   %r8
+            pushq   %rsi 
+            pushq   %rdi 
+            pushq   %rdx 
+            pushq   %rcx 
+            pushq   %rax
+            
+            inc     %rdi                # increment current_char_index
+            jmp     print_loop          # loop
+
         not_u:
 
         # if current_char == % ? handle it : jump to not_percent
@@ -136,9 +284,6 @@ my_printf:
         jmp     print_loop
     
     end_loop:
-
-    
-
 
     popq    %r15                    # restore all callee saved registers
     popq    %r15                    # restore all callee saved registers
@@ -184,34 +329,168 @@ print_char:         # write a single char that is stored in the least significan
     popq    %rbx                    # restore callee saved reg
     popq    %rbx                    #
 
-    jmp     epilogue_and_return
+        #   epilogue
+    movq    %rbp,   %rsp    # move SP back
+    popq    %rbp            # restore BP
+    ret                     # return
 
 
 #
 #   STRINGIFY_UNSIGNED_INT
 #   @params
 #       rdi -   an unsigned integer
-#       rsi -   the power of 10 for which the number should be returned
+#       rsi -   the starting address of a memory address on stack 24 bytes can be written to
 #
 #   @return
-#       rax -   char, (al) contains a single character (returns NUL char if the nth char is msb + 1)
+#       rax -   amount of bytes in the string
 #
 stringify_unsigned_int:
     # prologue
     pushq   %rbp
     movq    %rsp,   %rbp
 
-    movq    %rdi,   %rax
-    movq    %rdi,   %rdx
+    pushq   %r12            # save r12 (callee saved)
+    pushq   %r13            # save r13 (callee saved)
+    pushq   %r14            # save r14 (callee saved)
+    pushq   %r14            # save r14 (callee saved)
+    
+    xorq    %r12,   %r12    # clear r12
+
+    # if number == 0 ? push 0, skip loop : do loop
+    cmp     $0,     %rdi    
+    jne      BCD_loop
+
+    pushq   $0
+    inc     %r12                # if only one zero will be printed, r12 should still be one to account for the loop
+    jmp     post_loop
+
+    BCD_loop:
+        xorq    %rcx,   %rcx    #clr rcx
+        movq    $10,    %rcx    # set rcx equal to ten
+        inc     %r12            # increment the length of the number
+        #rdi contains number
+        movq    %rdi,   %rax    # rax will contain result
+        # rdx will contain remainder
+        xorq    %rdx,   %rdx    #clear rdx so div can use it 
+        div     %rcx            # div rdi by ten
+
+        movq    %rax,   %rdi    # move the rest of the decimal number back to rdi
+        pushq   %rdx            # remember the decimal we just figured out
+
+        # if number is accounted for ? turn it into string : reloop
+        cmp     $0,     %rdi
+        jne     BCD_loop
+    post_loop:
+    #   r12 contains the length
+
+    xorq    %r13,   %r13    # clear r13
+    xorq    %r8,    %r8      # clear r8, it will remember every 8th time a char is added because a push is then required
+    xorq    %r14,   %r14    # clear r14 (this will temporarily store the chars between pushes)
+    movq    %r12,   %rax    # move the length of the string to the output
+    
+    get_char_loop:
+
+        shl     $8,     %r14    # make space for next char (on first iteration this does nothing but thats ok)
+
+        popq    %r13            # r13 now contains a decimal digit
+        # ascii 0 = 48, so adding 48 to r13 will yield the ascii code of digits 0 thru 9
+        addq    $48,    %r13    # r13 now contains the ascii code
+
+        movb    %r13b,  %r14b   # mov char to r14
+        inc     %r8             # add 1 to r8
+
+        cmp     $8,     %r8
+        # if r8 == 8 ? push r14, clr r8 : continue
+        jne     after_push_buffer
+        # else mov r14 to spec addr,    clr r8
+
+        # i need to reorder the bytes so the endianess is reversed
+        # %r10 holds the new reordered reg
+        # %r11 the counter
+        xorq    %r10,   %r10        #clr r10
+        xorq    %r11,   %r11        #clr r11
+        endianess_loop1:
+            movb    %r14b,  %r10b       # move the last bit of r14 to the first bit of r11
+
+            inc     %r11                # increment r11
+            cmp     $8,     %r11
+            jge     end_endianess_loop1     
+            shr     $8,     %r14        # shift
+            shl     $8,     %r10        # shift
+            jmp     endianess_loop1
+
+        end_endianess_loop1:
+        movq    %r10,   %r14
+
+        movq    %r14,   (%rsi)  # write chars to %rsi
+        addq    $8,     %rsi    # subtract 8 from rsi so next push buffer will write to next 8 bytes
+        xorq    %r8,    %r8     # set r8 back to zero
+
+        after_push_buffer:
+
+        dec     %r12
+
+        cmp     $0,     %r12
+        # if i > 0 ? loop
+        jg      get_char_loop
+        # else step out of loop
+
+        #push remaining stuff
+        cmp     $0,     %r8
+        # if r8 is 0 -> dont do the next part
+        je      after_last_push
+        # else correctly shift the remaining bits and push 
+
+        movq    $8,     %r9
+        subq    %r8,    %r9
+        # r9 contains the shift index
+        mini_loop:              # so shift r14 left r9 times until 
+            dec     %r9
+            shl     $8,     %r14
+            cmp     $0,     %r9
+            jne     mini_loop
+
+        # i need to reorder the bytes so the endianess is reversed
+        # %r10 holds the new reordered reg
+        # %r11 the counter
+        xorq    %r10,   %r10        #clr r10
+        xorq    %r11,   %r11        #clr r11
+        endianess_loop:
+            movb    %r14b,  %r10b       # move the last byte of r14 to the first bit of r10
+
+            inc     %r11                # increment r11
+            cmp     $8,     %r11
+            jge     end_endianess_loop     
+            shr     $8,     %r14        # shift
+            shl     $8,     %r10        # shift
+            jmp     endianess_loop
+
+        end_endianess_loop:
+        movq    %r10,   %r14
+
+        movq    %r14,   (%rsi)
+        after_last_push:
+    
+    popq   %r14            #restore r14 (callee saved)    
+    popq   %r14            #restore r14 (callee saved)
+    popq   %r13            #restore r13 (callee saved)
+    popq   %r12            #restore r12 (callee saved)
+
+    # rax contains string length in bytes
+    movq    %rbp,   %rsp
+    popq    %rbp
+
+    ret        
+        
 
 #
 #   STRINGIFY_SIGNED_INT
 #   @params
 #       rdi -   a signed integer
-#       rsi -   the (n-1)th power of 10 for which the number should be returned (n = 0 yields sign)
+#       rsi -   the starting address of a memory address on stack 24 bytes can be written to
 #
 #   @return
-#       rax -   char, (al) contains a single character
+#       rax -   amount of bytes in the string
 #
 stringify_signed_int:
     #   prologue
@@ -228,15 +507,6 @@ stringify_signed_int:
     popq    %rdi                    # restore rdi
     popq    %rdi                    # restore rdi
 
-    cmp     $0,     %rsi            # check if argument is zero
-
-    # if rsi == 0 ? return sign : subtract one and return corresponding char
-    je      epilogue_and_return
-
-    # else
-    dec     %rsi
-
-
     cmp     $1,     %al             # check if sign bit is one
 
     # if sign == 0 ? jump positive : jump negative
@@ -247,13 +517,31 @@ stringify_signed_int:
         #   this is just the twos compliment math
         dec     %rdi                            # subtract 1 from rdi
         xorq    $0xFFFFFFFFFFFFFFFF,    %rdi    #xor with a string of ones will yield a flipped register
-    
+
+        # now put the negative sign on the reserved stack space and add one to the 
+        movb    $0x2D,  (%rsi)
+        addq    $1,     %rsi
+
+        call stringify_unsigned_int
+
+        addq    $1,     %rax        # rax contains the length of the string because of S_U_I but we need to add one because of the - char
+        
+        jmp     stringify_signed_epilogue
+
     stringify_positive:
         #   rdi now contains the unsigned positive counterpart
-    
-    # find the nth decimal digit based on rdi
+        #   rsi still contains starting addr
 
-    jmp epilogue_and_return
+        call    stringify_unsigned_int
+
+        # rax contains the length of the string written to the stack because thats what sui returns
+
+        jmp     stringify_signed_epilogue
+
+    stringify_signed_epilogue:
+    movq    %rbp,   %rsp    # move SP back
+    popq    %rbp            # restore BP
+    ret                     # return
     
 #
 #   ARG_COUNTER (not used)
@@ -325,7 +613,10 @@ arg_counter:        # run through the zero terminated string starting at the add
     popq    %r13                # restore callee saved regs
     popq    %r13                # restore callee saved regs
 
-    jmp epilogue_and_return
+        #   epilogue
+    movq    %rbp,   %rsp    # move SP back
+    popq    %rbp            # restore BP
+    ret                     # return
 
 #
 #   GET_ARGUMENT_N
@@ -364,8 +655,11 @@ get_argument_n:     # return the nth argument of my_printf (if n > the number of
     # if argument_n > 4 ? jump return_R9
     jg      return_STACK_item
 
-    # if fall-through ? jump epilogue_and_return
-    jmp     epilogue_and_return
+    # if fall-through ? epilogue and return
+        #   epilogue
+    movq    %rbp,   %rsp    # move SP back
+    popq    %rbp            # restore BP
+    ret                     # return
 
     return_RSI:
         # return RSI
@@ -401,9 +695,9 @@ get_argument_n:     # return the nth argument of my_printf (if n > the number of
 
         jmp     epilogue_and_return
     
-# as a shorthand to jump to
-epilogue_and_return:
-    #   epilogue
-    movq    %rbp,   %rsp    # move SP back
-    popq    %rbp            # restore BP
-    ret                     # return
+    # as a shorthand to jump to
+    epilogue_and_return:
+        #   epilogue
+        movq    %rbp,   %rsp    # move SP back
+        popq    %rbp            # restore BP
+        ret                     # return
